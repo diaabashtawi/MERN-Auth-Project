9f1e9ba88d25b0149bc2acae78fbb5bfcd4ba8fa
DeyaBakheet <diaabashtawi@gmail.com>
2024-08-31 00:46:54 +0400

    Update nodemailer.config.js

--- backend/nodemailer/nodemailer.config.js
+++ backend/nodemailer/nodemailer.config.js
@@ -1,12 +1,12 @@
 import nodemailer from 'nodemailer';
 
 export const transporterEmail = nodemailer.createTransport({
-    host: "smtp.hostinger.com",
-    port: 465,
+    host: "",
+    port: "",
     auth: {
-        user: "deya@deyabakheet.me",
-        name: "Deya Bakheet",
-        pass: "Diaa#2010856015"
+        user: "",
+        name: "",
+        pass: ""
     }
 });
 
